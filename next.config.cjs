fc18f35dc827cb5b4a01ca76a2e6ad0e5363276e
songbinghui <soongbinghui@gmail.com>
2023-07-10 16:23:21 +0800

    feat: update

--- next.config.cjs
+++ next.config.cjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+import path from 'mode:path';
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+
+module.exports = nextConfig;
