44b0980ee237f9827880fc7a364274040c70be85
songbinghui <soongbinghui@gmail.com>
2023-11-20 08:58:35 +0800

    feat: chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,5 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
-
 // Optional, but recommended: run on the edge runtime.
 // See https://vercel.com/docs/concepts/functions/edge-functions
 export const runtime = 'edge';
@@ -11,19 +10,27 @@ const openai = new OpenAI({
 });
 
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  const { messages, model } = await req.json();
+  try {
+    // Request the OpenAI API for the response based on the prompt
+    const response = await openai.chat.completions.create({
+      model: model ?? 'gpt-3.5-turbo',
+      stream: true,
+      messages,
+    });
 
-  // Request the OpenAI API for the response based on the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: messages,
-  });
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response as any);
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response as any);
-
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    // Check if the error is an APIError
+    if (error instanceof OpenAI.APIError) {
+      const { name, status, headers, message } = error;
+      return Response.json({ status, message }, { status });
+    } else {
+      throw error;
+    }
+  }
 }
